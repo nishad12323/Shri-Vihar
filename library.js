933d11a0f5611b0c82685b3940ee850c7e91c442
nishad12323 <155590890+nishad12323@users.noreply.github.com>
2024-01-04 02:55:21 +0000

    Stage all to import to github

--- library.js
+++ library.js
@@ -1,3 +1,16 @@
 function print(file) {
   window.open(file, "_self");
 }
+
+function register() {
+  location.assign("policies.html");
+}
+
+function share() {
+  navigator
+    .share({
+      files: ['beginning-ending/beginning-ending1.opus']
+    })
+    .then(() => console.log('Successful share! 🎉'))
+    .catch(err => console.error(err));
+}
